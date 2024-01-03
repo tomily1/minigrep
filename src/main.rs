142b264d155d3decf9b16daf24e8a8a0d78b4b15
Israel Tomilayo <itomilayo@gmail.com>
2024-01-03 18:53:46 -0500

    update main file to read sample file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+    // dbg!(&args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    let contents = fs::read_to_string(filename);
+
+    match contents {
+        Ok(contents) => println!("With text:\n{}", contents),
+        Err(e) => println!("Something went wrong: {}", e),
+    }
+        // .expect("Something went wrong reading the file");
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+    // println!("With text:\n{}", contents);
 }
