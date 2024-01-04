71f1c56c56e3520cf986ed2c1a94524185dced78
Israel Tomilayo <itomilayo@gmail.com>
2024-01-03 22:14:34 -0500

    make code much modular

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,21 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main(){
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
-        std::process::exit(1);
+        process::exit(1);
     });
-}
 
-#[derive(Debug)]
-struct Config {
-    query: String,
-    file_path: String,
-}
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-impl Config {
-    fn build(args: &[String]) -> Result<Self, &str> { // Note: if there is an error you might need to make this static. we just want to know why it would fail
-        if args.len() < 3 {
-            return Err("not enough arguments!");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Self { query, file_path })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
