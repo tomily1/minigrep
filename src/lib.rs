fd95bfdc1e1a173762bca1f64a19dc4c52ab123b
Israel Tomilayo <itomilayo@gmail.com>
2024-01-04 22:42:56 -0500

    make search case insenstive with environment variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub struct Config {
   pub query: String,
   pub file_path: String,
+  pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,22 +16,36 @@ impl Config {
 
       let query = args[1].clone();
       let file_path = args[2].clone();
+      let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-      Ok(Self { query, file_path })
+      Ok(Self { query, file_path, ignore_case })
   }
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   contents
     .lines()
-    .filter(|line| line.contains(query))
+    .filter(|line| line.contains(&query))
+    .collect()
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  contents
+    .lines()
+    .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
     .collect()
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let contents = fs::read_to_string(&config.file_path)?;
 
-  for line in search(&config.query, &contents) {
+  let results = if config.ignore_case {
+    search_case_insensitive(&config.query, &contents)
+  } else {
+    search(&config.query, &contents)
+  };
+
+  for line in results {
     println!("{}", line);
   }
 
@@ -41,7 +57,7 @@ mod tests {
   use super::*;
 
   #[test]
-  fn one_result() {
+  fn case_sensitive() {
     let query = "duct";
     let contents = "\
 Rust:
@@ -51,6 +67,21 @@ Pick three.";
     assert_eq!(
       vec!["safe, fast, productive."],
       search(query, contents)
+    );
+  }
+
+  #[test]
+  fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+    assert_eq!(
+      vec!["Rust:", "Trust me."],
+      search_case_insensitive(query, contents)
     );
   }
 }
